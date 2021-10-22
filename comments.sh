93035be56e94fbd1d7ed3a52646ae9876c0c75e9
fredrickwampamba <fredrickwampamba1@gmail.com>
2021-10-22 17:37:30 +0300

    multiple and single line commenting

---
+++ comments.sh
@@ -0,0 +1,14 @@
+#Single line commenting
+
+echo "the next line is a comment"
+#this will not be compiled for it  is a comment
+echo "Did you see that the line was not executed"
+
+echo 'testing if it is single or double qoute sensitve' #it actually doesnt mind
+
+#Multiple line commenting
+
+<<comment
+This is how we multile comment in shell programming
+comment
+echo "this line comes after multiple line commenting"
